b734ca97b8bad35646b758ecaba830ca49fa76ce
david940408 <david940408@gmail.com>
2014-08-06 14:15:25 +0800

    better paste,set locale in code

--- plot3.R
+++ plot3.R
@@ -1,16 +1,14 @@
 plot3<-function(){
     #put the unzipped txt file in the working directory
     
+    Sys.setlocale("LC_ALL","english")
     data<-read.table("household_power_consumption.txt",header = TRUE,sep = ";")
     data[,1]<-as.Date(data[,1],"%d/%m/%Y")
     data<-data[data[,1]>="2007-02-01"&data[,1]<="2007-02-02",]
     data$Sub_metering_1<-as.numeric(as.vector(data$Sub_metering_1))
     data$Sub_metering_2<-as.numeric(as.vector(data$Sub_metering_2))
     data$Sub_metering_3<-as.numeric(as.vector(data$Sub_metering_3))
-    date<-NULL
-    for(i in seq_len(nrow(data)))
-        date<-c(date,paste(data[i,1],data[i,2]))
-    date<-strptime(date,"%Y-%m-%d %H:%M:%S")
+    date<-strptime(paste(data[,1],data[,2]),"%Y-%m-%d %H:%M:%S")
     
     png("plot3.png",480,480,bg="transparent")
     plot(date,data$Sub_metering_1,type="n",main="",xlab="",
--- plot4.R
+++ plot4.R
@@ -1,6 +1,7 @@
 plot4<-function(){
     #put the unzipped txt file in the working directory
     
+    Sys.setlocale("LC_ALL","english")
     data<-read.table("household_power_consumption.txt",header = TRUE,sep = ";")
     data[,1]<-as.Date(data[,1],"%d/%m/%Y")
     data<-data[data[,1]>="2007-02-01"&data[,1]<="2007-02-02",]
@@ -10,10 +11,7 @@ plot4<-function(){
     data$Global_active_power<-as.numeric(as.vector(data$Global_active_power))
     data$Global_reactive_power<-as.numeric(as.vector(data$Global_reactive_power))
     data$Voltage<-as.numeric(as.vector(data$Voltage))
-    date<-NULL
-    for(i in seq_len(nrow(data)))
-        date<-c(date,paste(data[i,1],data[i,2]))
-    date<-strptime(date,"%Y-%m-%d %H:%M:%S")
+    date<-strptime(paste(data[,1],data[,2]),"%Y-%m-%d %H:%M:%S")
     
     
     png("plot4.png",480,480,bg="transparent")
